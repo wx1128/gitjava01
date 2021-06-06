4291433435e8131ebeae147fb3275676ec8fb390
zy <wx112820@163.com>
2021-06-06 23:01:17 +0800

    123

--- src/com/gj/dao/UserDao.java
+++ src/com/gj/dao/UserDao.java
@@ -1,17 +1,12 @@
 package com.gj.dao;
 
-import com.gj.entity.User;
-
-import java.util.List;
-
 /**
  * ClassName:UserDao
  * Package:com.gj.dao
  * Description:
  *
- * @Date:2021/6/6 22:43
+ * @Date:2021/6/6 22:50
  * @Author:wx112820@163.com
  */
 public interface UserDao {
-    List<User> selAllUser();
 }
--- src/com/gj/entity/User.java
+++ src/com/gj/entity/User.java
@@ -5,77 +5,9 @@ package com.gj.entity;
  * Package:com.gj.entity
  * Description:
  *
- * @Date:2021/6/6 22:28
+ * @Date:2021/6/6 22:49
  * @Author:wx112820@163.com
  */
 public class User {
-    private int id;
-//    用户名
-    private String userName;
-//    登录名
-    private  String loginName;
-//    登录密码
-    private String passWord;
-//    手机号码
-    private String phone;
-//    邮箱
-    private String email;
-//    性别
-    private int sex;
 
-    public int getSex() {
-        return sex;
-    }
-
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
